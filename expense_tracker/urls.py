c1efd99bae55516e5e5fc5695c4f4923fdedb9a2
priyanshu24 creation <chatpriyanshu60@gmail.com>
2026-01-27 23:27:00 +0530

    Fix login URL

--- expense_tracker/urls.py
+++ expense_tracker/urls.py
@@ -8,22 +8,12 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('login/', auth_views.LoginView.as_view(template_name="login.html"), name='login'),
-
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
-    path(
-        'password-change/',
-        auth_views.PasswordChangeView.as_view(template_name='password_change.html'),
-        name='password_change'
-    ),
-
-    path(
-        'password-change/done/',
-        auth_views.PasswordChangeDoneView.as_view(template_name='password_change_done.html'),
-        name='password_change_done'
-    ),
-
     path('', include('tracker.urls')),
+
+    path('password-change/', auth_views.PasswordChangeView.as_view(template_name='password_change.html'), name='password_change'),
+    path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='password_change_done.html'), name='password_change_done'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
